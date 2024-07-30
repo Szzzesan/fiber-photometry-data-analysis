f4de4bde3ff2a19ad32321c5045c4d255bcc5ab4
Szzzesan <98128431+Szzzesan@users.noreply.github.com>
2024-07-30 14:51:03 -0400

    Delete analysis_code directory

--- analysis_code/func/export_df_to_csv.py
+++
@@ -1,4 +0,0 @@
-def export_df_to_csv(my_df, my_path):
-    path = my_path + '_' + my_df.name + '.csv'
-    my_df.to_csv(path, index=False, sep=',')
-
--- analysis_code/func/find_closest_value.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def find_closest_value(s, x):
-    idx = (np.abs(s - x)).argmin()
-    return idx
