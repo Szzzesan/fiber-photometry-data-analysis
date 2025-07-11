8180fec4364e460f4a34acba147265091badbf98
Szzzesan <shichenzhang89@gmail.com>
2025-07-11 17:10:40 -0400

    dictionary of up-to-quality data for analysis

---
+++ quality_control.py
@@ -0,0 +1,23 @@
+qc_selections = {
+    'SZ036': {
+        'left',
+        'right'
+    },
+    'SZ037': {
+        'left',
+        'right'
+    },
+    'SZ038': {
+        'left',
+        'right'
+    },
+    'SZ039': {
+        'left'
+    },
+    'SZ042': {
+        'left'
+    },
+    'SZ043': {
+        'right'
+    }
+}
