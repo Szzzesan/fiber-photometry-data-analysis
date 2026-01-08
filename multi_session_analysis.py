612bc71b500f7a14accae9f0d2c93065697fc84e
Szzzesan <shichenzhang89@gmail.com>
2026-01-08 11:33:54 -0500

    sampling changes

--- multi_session_analysis.py
+++ multi_session_analysis.py
@@ -536,15 +536,14 @@ if __name__ == '__main__':
     # session_list = [0, 1, 2, 4, 11, 12, 13, 14, 15, 17, 18, 20, 22, 23, 24]
     # summary_RK6 = multi_session_analysis('RK006', session_list, include_branch='both', port_swap=1)
 
-    # session_list = [1, 2, 3, 5, 7, 9, 11, 12, 14, 15, 19, 22, 23, 24, 25]
-    # summary_036 = multi_session_analysis('SZ036', session_list, include_branch='both')
-    #
-    # session_list = [0, 1, 2, 4, 5, 6, 8, 9, 11, 15, 16, 17, 18, 19, 20, 22, 24, 25, 27, 28, 29, 31, 32, 33, 35]
-    # summary_037 = multi_session_analysis('SZ037', session_list, include_branch='both')
+    session_list = [1, 2, 3, 5, 7, 9, 11, 12, 14, 15, 19, 22, 23, 24, 25]
+    summary_036 = multi_session_analysis('SZ036', session_list, include_branch='both')
 
-    # session_list = [1, 2, 3, 4, 7, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 29, 30, 32,
-    #                 33, 34, 35, 36, 37, 38]
-    session_list = [35, 36, 37, 38]
+    session_list = [0, 1, 2, 4, 5, 6, 8, 9, 11, 15, 16, 17, 18, 19, 20, 22, 24, 25, 27, 28, 29, 31, 32, 33, 35]
+    summary_037 = multi_session_analysis('SZ037', session_list, include_branch='both')
+
+    session_list = [1, 2, 3, 4, 7, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 29, 30, 32,
+                    33, 34, 35, 36, 37, 38]
     summary_038 = multi_session_analysis('SZ038', session_list, include_branch='both')
 
     session_list = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 22]
